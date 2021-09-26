b00872744b95454708e9f881d9fd4b66e3a829dc
Felipe Carosella <carosellafelipe20@gmail.com>
2021-09-25 22:48:08 -0300

    ac

--- sketch.js
+++ sketch.js
@@ -41,7 +41,7 @@ function initMap(lati,lon){
     }
     myMap = mappa.tileMap(options);
     myMap.overlay(canvas);
-    L.marker([lati, lon]).addTo(map)
+    myMap.marker([lati, lon]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
 }
