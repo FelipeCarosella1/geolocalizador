49ebd0e4448527823fbe023933577edfebf6cf3d
Felipe Carosella <carosellafelipe20@gmail.com>
2021-09-25 22:12:26 -0300

    ac

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 
 let lat; // variable de latitud
 let lon; // variable de longitud
+let canvas;
 function setup() {
     if('geolocation' in navigator) {
         /* geolocation is available */
@@ -18,7 +19,7 @@ function setup() {
         console.log('geolocation NO funcionando');
     };
 
-createCanvas(200,200); // crea el linezo de 200x200
+canvas = createCanvas(200,200); // crea el linezo de 200x200
 initMap()
   }
   
@@ -31,154 +32,14 @@ text(lon,85,50); // imprime variable en posicion x,y
   }
 
 function initMap() {
-    const key = 'xyz';
-    const style = [{
-  elementType: 'geometry',
-  stylers: [{
-    color: '#242f3e',
-  }],
-}, {
-  elementType: 'labels.text.fill',
-  stylers: [{
-    color: '#746855',
-  }],
-}, {
-  elementType: 'labels.text.stroke',
-  stylers: [{
-    color: '#242f3e',
-  }],
-}, {
-  'featureType': 'administrative.land_parcel',
-  stylers: [{
-    'visibility': 'off'
-  }]
-}, {
-  'featureType': 'administrative.locality',
-  elementType: 'labels.text.fill',
-  stylers: [{
-    color: '#d59563'
-  }]
-}, {
-  'featureType': 'administrative.neighborhood',
-  stylers: [{
-    'visibility': 'off'
-  }]
-}, {
-  'featureType': 'poi',
-  elementType: 'labels.text',
-  stylers: [{
-    'visibility': 'off'
-  }]
-}, {
-  'featureType': 'poi',
-  elementType: 'labels.text.fill',
-  stylers: [{
-    color: '#d59563'
-  }]
-}, {
-  'featureType': 'poi.park',
-  elementType: 'geometry',
-  stylers: [{
-    color: '#263c3f'
-  }]
-}, {
-  'featureType': 'poi.park',
-  elementType: 'labels.text.fill',
-  stylers: [{
-    color: '#6b9a76'
-  }]
-}, {
-  'featureType': 'road',
-  stylers: [{
-    'visibility': 'off'
-  }]
-}, {
-  'featureType': 'road',
-  elementType: 'geometry',
-  stylers: [{
-    color: '#38414e'
-  }]
-}, {
-  'featureType': 'road',
-  elementType: 'geometry.stroke',
-  stylers: [{
-    color: '#212a37'
-  }]
-}, {
-  'featureType': 'road',
-  elementType: 'labels',
-  stylers: [{
-    'visibility': 'off'
-  }]
-}, {
-  'featureType': 'road',
-  elementType: 'labels.text.fill',
-  stylers: [{
-    color: '#9ca5b3'
-  }]
-}, {
-  'featureType': 'road.highway',
-  elementType: 'geometry',
-  stylers: [{
-    color: '#746855'
-  }]
-}, {
-  'featureType': 'road.highway',
-  elementType: 'geometry.stroke',
-  stylers: [{
-    color: '#1f2835'
-  }]
-}, {
-  'featureType': 'road.highway',
-  elementType: 'labels.text.fill',
-  stylers: [{
-    color: '#f3d19c'
-  }]
-}, {
-  'featureType': 'transit',
-  elementType: 'geometry',
-  stylers: [{
-    color: '#2f3948'
-  }]
-}, {
-  'featureType': 'transit.station',
-  elementType: 'labels.text.fill',
-  stylers: [{
-    color: '#d59563'
-  }]
-}, {
-  'featureType': 'water',
-  elementType: 'geometry',
-  stylers: [{
-    color: '#17263c'
-  }]
-}, {
-  'featureType': 'water',
-  elementType: 'labels.text',
-  stylers: [{
-    'visibility': 'off'
-  }]
-}, {
-  'featureType': 'water',
-  elementType: 'labels.text.fill',
-  stylers: [{
-    color: '#515c6d'
-  }]
-}, {
-  'featureType': 'water',
-  elementType: 'labels.text.stroke',
-  stylers: [{
-    color: '#17263c'
-  }]
-}];
     let myMap;
-    const mappa = new Mappa('Google', key);
+    const mappa = new Mappa('Leaflet');;
     const options = {
-    lat: 2,
-    lng: 2,
-    zoom: 3,
-    styles: style,
-};
+    lat: 0,
+    lng: 0,
+    zoom: 4,
+    style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    }
     myMap = mappa.tileMap(options);
     myMap.overlay(canvas);
 }
