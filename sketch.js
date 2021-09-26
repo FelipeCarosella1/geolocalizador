a360b31e196738332e99ab0bde3a338acb1c09d6
Felipe Carosella <carosellafelipe20@gmail.com>
2021-09-25 23:17:25 -0300

    a

--- sketch.js
+++ sketch.js
@@ -37,7 +37,7 @@ function initMap(lati,lon){
     const options = {
     lat: lati,
     lng: lon,
-    zoom: 4,
+    zoom: 10,
     style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
     }
     myMap = mappa.tileMap(options);
