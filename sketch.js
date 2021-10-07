83ccda635df40fa7b0362a51d4f4f04788a458fd
Felipe Carosella <carosellafelipe20@gmail.com>
2021-10-07 12:10:07 -0300

    ac

--- sketch.js
+++ sketch.js
@@ -57,8 +57,41 @@ function marcador(){
     image(imagen,marcador.x,marcador.y,35,35);
 }
 
-function leerArchivo() {
-    var fs = require('fs'); 
-    var data = fs.readFileSync('cordenadas.csv');
-    console.log(data)
+function parseCSV(text) {
+  // Obtenemos las lineas del texto
+  let lines = text.replace(/\r/g, '').split('\n');
+  return lines.map(line => {
+    // Por cada linea obtenemos los valores
+    let values = line.split(',');
+    return values;
+  });
 }
+
+function reverseMatrix(matrix){
+  let output = [];
+  // Por cada fila
+  matrix.forEach((values, row) => {
+    // Vemos los valores y su posicion
+    values.forEach((value, col) => {
+      // Si la posición aún no fue creada
+      if (output[col] === undefined) output[col] = [];
+      output[col][row] = value;
+    });
+  });
+  return output;
+}
+
+function readFile(evt) {
+  let file = evt.target.files[0];
+  let reader = new FileReader();
+  reader.onload = (e) => {
+    // Cuando el archivo se terminó de cargar
+    let lines = parseCSV(e.target.result);
+    let output = reverseMatrix(lines);
+    console.log(output);
+  };
+  // Leemos el contenido del archivo seleccionado
+  reader.readAsBinaryString(file);
+}
+
+document.getElementById('file').addEventListener('change', readFile, false);
