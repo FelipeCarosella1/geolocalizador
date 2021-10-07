41a371679ea6c1ede7ba084f1e2d65801304afe6
Felipe Carosella <carosellafelipe20@gmail.com>
2021-10-07 13:38:29 -0300

    ac

--- sketch.js
+++ sketch.js
@@ -57,3 +57,29 @@ function marcador(){
     image(imagen,marcador.x,marcador.y,35,35);
 }
 
+$(document).ready(function() { 
+    $.ajax({
+        type: "GET", 
+        url: "data.txt", 
+        dataType: "text", 
+        success: function(data) {processData(data);} 
+    }); 
+}); 
+
+function processData(allText) { 
+    var allTextLines = allText.split(/\r\n|\n/); 
+    var headers = allTextLines[0].split(','); 
+    var lines = []; 
+    for (var i=1; i<allTextLines.length; i++) { 
+        var data = allTextLines[i].split(','); 
+        if (data.length == headers.length) { 
+            var tarr = []; 
+            for (var j=0; j<headers.length; j++) { 
+                tarr.push(headers[j]+":"+data[j]); 
+            } lines.push(tarr); 
+        } 
+    } // alert(lines); 
+    print(lines)
+}
+
+
