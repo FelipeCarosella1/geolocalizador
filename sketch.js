ef1eba2cc2b5e36fa88455daf8b0d25f302ba196
Felipe Carosella <carosellafelipe20@gmail.com>
2021-10-07 15:42:42 -0300

    ac

--- sketch.js
+++ sketch.js
@@ -45,8 +45,8 @@ text(lon,85,50); // imprime variable en posicion x,y
 function initMap(){
     const mappa = new Mappa('Leaflet');;
     const options = {
-    lat: 47.1443,
-    lng: -122.1408,
+    lat: -34.6075682,
+    lng: -58.4370894,
     zoom: 8,
     style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
     }
