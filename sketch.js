f1c9095cc683867a0d0001d529a1548b1f9d4508
Patricia Gonella <carosellafelipe@gmail.com>
2021-10-12 17:37:45 -0300

    sd

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let datos; // variable que almacena los datos del archivo CSV
 let imagen;
 let lat;
 let lon;
-
+let img;
 
 
 function preload() {
@@ -63,9 +63,9 @@ function marcador(){
     clear()
     let numRows = datos.getRowCount(); // almacena las filas como datos
     // almacenamos altitud y longitus en una matriz
-    let lat = datos.getColumn("lat"); // usamos el nombre que figura en al tabla exel CSV
-    let lon = datos.getColumn("lon"); // usamos el nombre que figura en la tabla exel CSV 
-    let img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exel CSV
+    lat = datos.getColumn("lat"); // usamos el nombre que figura en al tabla exel CSV
+    lon = datos.getColumn("lon"); // usamos el nombre que figura en la tabla exel CSV 
+    img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exel CSV
     // ciclo repetitivo que recorra todos los datos desde 0 hasta el valor de menor de filas 
     for (let i = 0; i < numRows; i++) {
         imagen=createImg(img[i]);
@@ -87,9 +87,9 @@ let img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exe
 let newRow = table.addRow();
 let numRows = datos.getRowCount(); // almacena las filas como datos
 // almacenamos altitud y longitus en una matriz
-let lat = datos.getColumn("lat"); // usamos el nombre que figura en al tabla exel CSV
-let lon = datos.getColumn("lon"); // usamos el nombre que figura en la tabla exel CSV 
-let img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exel CSV
+lat = datos.getColumn("lat"); // usamos el nombre que figura en al tabla exel CSV
+lon = datos.getColumn("lon"); // usamos el nombre que figura en la tabla exel CSV 
+img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exel CSV
 // ciclo repetitivo que recorra todos los datos desde 0 hasta el valor de menor de filas 
 for (let i = 0; i < numRows; i++) {
     newRow.setNum('lat', lat[i]);
